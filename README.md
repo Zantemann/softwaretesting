2cc98ff3bab6f5df65c5218b199dd01fbe20430f
Santeri <santeri.ora@tuni.fi>
2024-11-15 21:44:51 +0200

    Coveralls to pipeline

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Coverage Status](https://coveralls.io/repos/github/Zantemann/softwaretesting/badge.svg?branch=main)](https://coveralls.io/github/Zantemann/softwaretesting?branch=main)
--- tests/setup.test.js
+++ tests/setup.test.js
@@ -1,7 +1,26 @@
 import { assert } from "chai";
+import map from "../src/map.js";
 
 describe("Pipeline Test", () => {
   it("should pass this placeholder test", () => {
     assert.isTrue(true);
+  });
+
+  it("should map array values using the iteratee function", () => {
+    function square(n) {
+      return n * n;
+    }
+
+    const result = map([4, 8], square);
+    assert.deepEqual(result, [16, 64]);
+  });
+
+  it("should return an empty array when input is null or undefined", () => {
+    function square(n) {
+      return n * n;
+    }
+
+    assert.deepEqual(map(null, square), []);
+    assert.deepEqual(map(undefined, square), []);
   });
 });
